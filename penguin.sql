3397dd9285dd94a25792a56f8f69ba7fecc22027
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2023-09-20 17:03:32 +0700

    upload penguin.sql

---
+++ penguin.sql
@@ -0,0 +1,22 @@
+create table public.user (
+	id SERIAL primary key,
+	username TEXT,
+	name TEXT,
+	email TEXT,
+	phone TEXT,
+	birthday date,
+	gender boolean
+)
+
+create table public.post (
+	id SERIAL primary key,
+	content TEXT,
+	authorId int references user(id)
+)
+
+create table public.comment (
+	id SERIAL primary key,
+	content TEXT,
+	authorId int references public.user(id),
+	postId int references public.post(id)
+)
